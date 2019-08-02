da4475cb2338765fdd13df4907c0412dc48ad83c
wfsch <bschiela@gmail.com>
2019-08-02 14:14:25 -0400

    Fix compiler warnings from unused variables

--- GetMinimumIndexInRange.m
+++ GetMinimumIndexInRange.m
@@ -2,6 +2,6 @@ function [n] = GetMinimumIndexInRange(x, y, x0, x1)
     a = GetIndex(x, x0);
     b = GetIndex(x, x1);
     
-    [miny, n] = min(y(a : b));
+    [~, n] = min(y(a : b));
     n = a + n;
 end
--- ZeroFieldIndex.m
+++ ZeroFieldIndex.m
@@ -1,3 +1,3 @@
 function [n] = ZeroFieldIndex(B)
-    [x, n] = min(abs(B));
+    [~, n] = min(abs(B));
 end
